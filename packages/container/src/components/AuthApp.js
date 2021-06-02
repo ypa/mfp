abe8c449db43b5ecf9cbf210af7fbb590f4dac93
Yan P Aung <yan.pye.aung@gmail.com>
2021-06-01 20:30:48 -0700

    Pass down onSignIn method from container down to AuthApp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -16,6 +16,9 @@ export default () => {
           history.push(nextPathname);
         }
       },
+      onSignIn: () => {
+        console.log('User signed in');
+      },
     });
 
     history.listen(onParentNavigate);
